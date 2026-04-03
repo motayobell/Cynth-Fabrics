f405fda61d2f0fc63b7aad20c919a8057214b06f
motayobell <97890923+motayobell@users.noreply.github.com>
2026-04-03 20:23:25 +0100

    feat: Use PORT from environment variable
    Allows the server to be deployed to platforms that dynamically assign ports.

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ const upload = multer({
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
